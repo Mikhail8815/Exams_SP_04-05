f5aaf0b9a96caae9b4588b1bcfd857d9946c6b60
Mikhail8815 <mishutka150588@mail.ru>
2025-05-05 23:19:40 +0300

    SP_05_week_02 (done)

---
+++ src/main.tsx
@@ -0,0 +1,82 @@
+import { configureStore } from "@reduxjs/toolkit"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux"
+
+type Comment = {
+  postId: string
+  id: string
+  name: string
+  email: string
+  body: string
+}
+
+// Api
+const api = createApi({
+  reducerPath: "commentsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://exams-frontend.kimitsu.it-incubator.io/api/" }),
+  endpoints: (builder) => {
+    return {
+      getComments: builder.query<Comment[], void>({
+        query: () => "comments",
+      }),
+      addComment: builder.mutation<Comment, { body: string }>({
+        query: (com) => ({
+          url: '/comments',
+          method: 'POST',
+          body: { body: com }
+        }),
+      })
+      // ❗❗❗XXX❗❗❗
+    }
+  },
+})
+
+const { useGetCommentsQuery, useAddCommentMutation } = api
+
+// App.tsx
+const App = () => {
+  const { data } = useGetCommentsQuery()
+  const [addComment] = useAddCommentMutation()
+
+  const addCommentHandler = () => {
+    addComment("Тестовая строка. Ее менять не нужно")
+  }
+
+  return (
+    <>
+      <button onClick={addCommentHandler}>Add comment</button>
+      {data?.map((el) => {
+        return (
+          <div key={el.id} style={{ border: "1px solid", margin: "5px", padding: "5px" }}>
+            <p>body - {el.body}</p>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+// store.ts
+const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+})
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
+
+// 📜 Описание:
+// Белый экран. Откройте панель разработчика и проанализируйте в чем ошибка
+
+// 🪛 Задача:
+// Что нужно написать вместо `// ❗❗❗XXX❗❗❗` чтобы при нажатии на кнопку `Add comment`
+// новый комментарий добавлялся и был виден в конце массива после перезагрузки страница
+// ❗ Автоматическое получение данных реализовывать не надо
+// В качестве ответа укажите написанный вами код
+// ❗Типизацию указывать обязательно
